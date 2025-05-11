84c3f75d87f4f2ec444e236043502a28b762c4ab
AljoharaAbdulhakeem <aljoharabindokhi23@gmail.com>
2025-05-11 11:58:52 +0300

    Add files via upload

---
+++ CollageRepostry.cs
@@ -0,0 +1,33 @@
+﻿namespace JWT_Token.Model
+{
+    public static class CollageRepostry
+    {
+        public static List<Student> Student { get; set; } = new List<Student>()
+        {
+            new Student
+             {
+                 Id = 1,
+                 Name = "ALjohara",
+                 Email = "Aljohara1@gmail.com",
+                 Address ="Alhzem,Jeedah"
+             },
+
+             new Student
+             {
+                 Id = 2,
+                 Name = "Sara",
+                 Email = "Sara@gmail.com",
+                 Address ="Alyasmeen,Ryaidh"
+             },
+
+             new Student
+             {
+                 Id = 3,
+                 Name = "Noura",
+                 Email = "Noura@gmail.com",
+                 Address ="Hiteen,Ryaidh"
+
+             }
+        };
+    }
+}
